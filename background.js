5731ccd25ba67cf37f8a9efeae7af932eb9e0064
667 <667@june07.com>
2016-11-19 10:57:27 -0800

    no message

--- background.js
+++ background.js
@@ -23,154 +23,170 @@
  */
 var ngApp = angular.module('NimBackgroundApp', []);
 ngApp
-  .run(function() {})
-  .controller('nimController', ['$scope', '$window', '$http', function($scope, $window, $http) {
-    $scope.loaded = Date.now();
-    $scope.timer = 0;
-    $scope.auto = false;
-    $scope.timerInterval = 1000;
-    $scope.checkInterval = 5;
-    $scope.checkIntervalTimeout = null;
-    $scope.debug = false;
-    $scope.newWindow = false;
-    $scope.autoClose = true;
-    $scope.devtoolsActive = false;
-    $scope.devToolsSessions = [];
+    .run(function () {})
+    .controller('nimController', ['$scope', '$window', '$http', function ($scope, $window, $http) {
+        const UPTIME_CHECK_INTERVAL = 1000 * 60 * 15; // 15 minutes
+        $scope.loaded = Date.now();
+        $scope.timer = 0;
+        $scope.auto = false;
+        $scope.timerInterval = 1000;
+        $scope.checkInterval = 5;
+        $scope.checkIntervalTimeout = null;
+        $scope.debug = false;
+        $scope.newWindow = false;
+        $scope.autoClose = true;
+        $scope.tabActive = false;
+        $scope.windowFocused = false;
+        $scope.devToolsSessions = [];
 
-    var chrome = $window.chrome;
-    setInterval(function() {
-      $scope.timer++;
-    }, $scope.timerInterval);
+        var chrome = $window.chrome;
+        $scope.moment = $window.moment;
 
-    $scope.$on('options-window-closed', function() {
-      resetInterval($scope.checkIntervalTimeout);
-    });
-    var resetInterval = function(timeout) {
-      if (timeout) clearInterval(timeout);
-      $scope.checkIntervalTimeout = setInterval(function() {
-        if ($scope.auto) {
-          $scope.closeDevTools();
-          $scope.openTab($scope.host, $scope.port, function(result) {
-            $scope.message = result;
-          });
-        }
-      }, $scope.checkInterval * 1000);
-    }
-    resetInterval();
+        setInterval(function () {
+            $scope.timer++;
+            if ($scope.timer >= UPTIME_CHECK_INTERVAL && $scope.timer%UPTIME_CHECK_INTERVAL === 0) {
+                $window._gaq.push(['_trackEvent', $scope.moment.duration($scope.timer, 'seconds').humanize(), 'Uptime Checked']);
+            }
+        }, $scope.timerInterval);
 
-    $scope.closeDevTools = function() {
-      $scope.devToolsSessions.forEach(function(devToolsSession, index) {
-        if (devToolsSession.autoClose) {
-          $http({
-              method: "GET",
-              url: devToolsSession.infoUrl,
-              responseType: "json"
-            })
-            .catch(function(error) {
-              if (error.status == -1) {
-                if (! devToolsSession.isWindow) {
-                  chrome.tabs.remove(devToolsSession.id, function() {
-                      $scope.devToolsSessions.splice(index, 1);
-                    $scope.message += '<br>Closed tab for DevTools session: ' + JSON.stringify(devToolsSession) + '.';
-                  });
-                } else {
-                  chrome.windows.remove(devToolsSession.id, function() {
-                      $scope.devToolsSessions.splice(index, 1);
-                    $scope.message += '<br>Closed window for DevTools session: ' + JSON.stringify(devToolsSession) + '.';
-                  });
+        $scope.$on('options-window-closed', function () {
+            resetInterval($scope.checkIntervalTimeout);
+        });
+
+        function resetInterval(timeout) {
+            if (timeout) {
+                clearInterval(timeout);
+            }
+            $scope.checkIntervalTimeout = setInterval(function () {
+                if ($scope.auto) {
+                    $scope.closeDevTools();
+                    $scope.openTab($scope.host, $scope.port, function (result) {
+                        $scope.message = result;
+                    });
                 }
-              } else {
-                $scope.message += '<br>Error while trying to auto close ' + (devToolsSession.isWindow ? 'window' : 'tab') + error;
-              }
+            }, $scope.checkInterval * 1000);
+        }
+        resetInterval();
+
+        $scope.closeDevTools = function () {
+            $scope.devToolsSessions.forEach(function (devToolsSession, index) {
+                if (devToolsSession.autoClose) {
+                    $http({
+                            method: "GET",
+                            url: devToolsSession.infoUrl,
+                            responseType: "json"
+                        })
+                        .catch(function (error) {
+                            if (error.status === -1) {
+                                if (!devToolsSession.isWindow) {
+                                    chrome.tabs.remove(devToolsSession.id, function () {
+                                        $scope.devToolsSessions.splice(index, 1);
+                                        $scope.message += '<br>Closed tab for DevTools session: ' + JSON.stringify(devToolsSession) + '.';
+                                    });
+                                } else {
+                                    chrome.windows.remove(devToolsSession.id, function () {
+                                        $scope.devToolsSessions.splice(index, 1);
+                                        $scope.message += '<br>Closed window for DevTools session: ' + JSON.stringify(devToolsSession) + '.';
+                                    });
+                                }
+                            } else {
+                                $scope.message += '<br>Error while trying to auto close ' + (devToolsSession.isWindow ? 'window' : 'tab') + error;
+                            }
+                        });
+                }
+            });
+        };
+        $scope.openTab = function (host, port, callback) {
+            var infoUrl = 'http://' + $scope.host + ':' + $scope.port + '/json';
+            chrome.tabs.query({
+                url: 'https://chrome-devtools-frontend.appspot.com/*' + host + ':' + port + '*'
+            }, function (tab) {
+                if (tab.length === 0) {
+                    $http({
+                            method: "GET",
+                            url: infoUrl,
+                            responseType: "json"
+                        })
+                        .then(function openDevToolsFrontend(json) {
+                            var url = json.data[0].devtoolsFrontendUrl.replace("localhost:9229", host + ":" + port);
+                            /** May be a good idea to put this somewhere further along the chain in case tab/window creation fails,
+                            in which case this entry will need to be removed from the array */
+                            createTabOrWindow(infoUrl, url, callback);
+                        })
+                        .catch(function (error) {
+                            if (error.status === -1) {
+                                var message =
+                                    'Connection to DevTools host was aborted.  Check your host and port.';
+                                callback(message);
+                            } else {
+                                callback(error);
+                            }
+                        });
+                } else {
+                    callback("DevTools is already open.");
+                }
+            });
+        };
+
+        function createTabOrWindow(infoUrl, url, callback) {
+            if ($scope.newWindow) {
+                chrome.windows.create({
+                    url: url,
+                    focused: $scope.windowFocused,
+                }, function (window) {
+                    saveSession(infoUrl, window.id);
+                    callback(window.url);
+                });
+            } else {
+                chrome.tabs.create({
+                    url: url,
+                    active: $scope.tabActive,
+                }, function (tab) {
+                    saveSession(infoUrl, tab.id);
+                    callback(tab.url);
+                });
+            }
+        }
+
+        function saveSession(infoUrl, id) {
+            $scope.devToolsSessions.push({
+                autoClose: $scope.autoClose,
+                isWindow: $scope.newWindow,
+                infoUrl: infoUrl,
+                id: id
             });
         }
-      });
-    }
-    $scope.openTab = function(host, port, callback) {
-      var infoUrl = 'http://' + $scope.host + ':' + $scope.port + '/json';
-      chrome.tabs.query({
-        url: 'https://chrome-devtools-frontend.appspot.com/*' + host + ':' + port + '*'
-      }, function(tab) {
-        if (tab.length == 0) {
-          $http({
-              method: "GET",
-              url: infoUrl,
-              responseType: "json"
-            })
-            .then(function openDevToolsFrontend(json) {
-              var url = json['data'][0]['devtoolsFrontendUrl'].replace("localhost:9229", host + ":" + port);
-              /** May be a good idea to put this somewhere further along the chain in case tab/window creation fails,
-              in which case this entry will need to be removed from the array */
-              createTabOrWindow(infoUrl, url, callback);
-            })
-            .catch(function(error) {
-              if (error.status == -1) {
-                var message =
-                  'Connection to DevTools host was aborted.  Check your host and port.';
-                callback(message);
-              } else {
-                callback(error);
-              }
-            });
-        } else {
-          callback("DevTools is already open.");
-        }
-      });
-    }
-    var createTabOrWindow = function(infoUrl, url, callback) {
-      if ($scope.newWindow) {
-        chrome.windows.create({
-          url: url,
-          focused: $scope.devtoolsActive,
-      }, function(window) {
-          saveSession(infoUrl, window.id);
-          callback(window.url);
+        $scope.save = function (key, obj) {
+            switch (key) {
+            case "host":
+                chrome.storage.sync.set({
+                    host: obj
+                }, function () {
+                    if ($scope.debug) {
+                        console.log("saved host: " + obj);
+                    }
+                });
+                break;
+            case "port":
+                chrome.storage.sync.set({
+                    port: obj
+                }, function () {
+                    if ($scope.debug) {
+                        console.log("saved port: " + obj);
+                    }
+                });
+                break;
+            }
+        };
+        $scope.s = function (key) {
+            $scope.save(key, $scope[key]);
+        };
+        chrome.storage.sync.get("host", function (obj) {
+            $scope.host = obj.host || "localhost";
+            $scope.$apply();
         });
-      } else {
-        chrome.tabs.create({
-          url: url,
-          active: $scope.devtoolsActive,
-        }, function(tab) {
-          saveSession(infoUrl, tab.id);
-          callback(tab.url);
+        chrome.storage.sync.get("port", function (obj) {
+            $scope.port = obj.port || 9229;
+            $scope.$apply();
         });
-      }
-    }
-    var saveSession = function(infoUrl, id) {
-      $scope.devToolsSessions.push({
-        autoClose: $scope.autoClose,
-        isWindow: $scope.newWindow,
-        infoUrl: infoUrl,
-        id: id
-      });
-    }
-    $scope.save = function(key, obj) {
-      switch (key) {
-        case "host":
-          chrome.storage.sync.set({
-            host: obj
-          }, function() {
-            if ($scope.debug) console.log("saved host: " + obj);
-          });
-          break;
-        case "port":
-          chrome.storage.sync.set({
-            port: obj
-          }, function() {
-            if ($scope.debug) console.log("saved port: " + obj);
-          });
-          break;
-      }
-    }
-    $scope.s = function(key) {
-      $scope.save(key, $scope[key]);
-    }
-    chrome.storage.sync.get("host", function(obj) {
-      $scope.host = obj.host || "localhost";
-      $scope.$apply();
-    });
-    chrome.storage.sync.get("port", function(obj) {
-      $scope.port = obj.port || 9229;
-      $scope.$apply();
-    });
   }]);
--- options.js
+++ options.js
@@ -23,46 +23,50 @@
  */
 var ngApp = angular.module('NimOptionsApp', []);
 ngApp
-  .run(function() {})
-  .controller('nimOptionsController', ['$scope', '$window', '$http', function($scope, $window, $http) {
-    $scope.bg = $window.chrome.extension.getBackgroundPage().angular.element('#nim').scope();
-    $scope.auto = $scope.bg.auto;
-    $scope.debug = $scope.bg.debug;
-    $scope.newWindow = $scope.bg.newWindow;
-    $scope.autoClose = $scope.bg.autoClose;
-    $scope.checkInterval = $scope.bg.checkInterval;
+    .run(function () {})
+    .controller('nimOptionsController', ['$scope', '$window', '$http', function ($scope, $window) {
+        $scope.bg = $window.chrome.extension.getBackgroundPage().angular.element('#nim').scope();
+        $scope.auto = $scope.bg.auto;
+        $scope.debug = $scope.bg.debug;
+        $scope.newWindow = $scope.bg.newWindow;
+        $scope.autoClose = $scope.bg.autoClose;
+        $scope.checkInterval = $scope.bg.checkInterval;
+        $scope.tabActive = $scope.bg.tabActive;
+        $scope.windowFocused = $scope.bg.windowFocused;
 
-    var chrome = $window.chrome;
-    var slider = $window.document.getElementById('checkInterval');
-    $window.noUiSlider.create(slider, {
-      start: [$scope.checkInterval],
-      step: 1,
-      range: {
-        'min': [ 5 ],
-        'max': [ 300 ]
-      },
-      tooltips: true
-    });
-    var rangeSliderValueElement = document.getElementById('checkInterval-value');
-    slider.noUiSlider.on('update', function( values, handle ) {
-  	     rangeSliderValueElement.innerHTML = values[handle];
-         $scope.bg.checkInterval = parseInt(values[handle]);
-     });
-     slider.noUiSlider.on('set', function( values, handle ) {
-          _gaq.push(['_trackEvent', 'checkInterval-value', values[handle]]);
-      });
-     $scope.saveButtonHandler = function() {
-          _gaq.push(['_trackEvent', 'save button', 'clicked']);
-         $scope.bg.$emit('options-window-closed');
-         $window.close();
-     }
-     function trackInputClick(e) {
-         _gaq.push(['_trackEvent', e.target.id, 'clicked']);
-     }
-     var userInputs = $window.document.getElementsByClassName('ga-track');
-     for (var i = 0; i < userInputs.length; i++) {
-         userInputs[i].addEventListener('click', function (event) {
-             trackInputClick(event);
-         });
-     }
-  }]);
+        var slider = $window.document.getElementById('checkInterval');
+        $window.noUiSlider.create(slider, {
+            start: [$scope.checkInterval],
+            step: 1,
+            range: {
+                'min': [5],
+                'max': [300]
+            },
+            tooltips: true
+        });
+        var rangeSliderValueElement = $window.document.getElementById('checkInterval-value');
+        slider.noUiSlider.on('update', function (values, handle) {
+            rangeSliderValueElement.innerHTML = values[handle];
+            $scope.bg.checkInterval = parseInt(values[handle]);
+        });
+        slider.noUiSlider.on('set', function (values, handle) {
+            $window._gaq.push(['_trackEvent', 'checkInterval-value', values[handle]]);
+        });
+        $scope.saveButtonHandler = function () {
+            $window._gaq.push(['_trackEvent', 'save button', 'clicked']);
+            $scope.bg.$emit('options-window-closed');
+            $window.close();
+        };
+
+        function trackInputClick(e) {
+            $window._gaq.push(['_trackEvent', e.target.id, 'clicked']);
+        }
+        var userInputs = $window.document.getElementsByClassName('ga-track');
+
+        function trackInputClickListener(event) {
+            trackInputClick(event);
+        }
+        for (var i = 0; i < userInputs.length; i++) {
+            userInputs[i].addEventListener('click', trackInputClickListener);
+        }
+    }]);
