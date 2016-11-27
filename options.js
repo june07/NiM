fed8b82eb942401362c97d30751188c39fe1b470
667 <667@june07.com>
2016-11-26 18:07:58 -0800

    Version 6.0

--- options.js
+++ options.js
@@ -24,11 +24,11 @@
 var ngApp = angular.module('NimOptionsApp', ['ngAnimate']);
 ngApp
     .run()
-    .controller('nimOptionsController', ['$scope', '$window', function ($scope, $window) {
+    .controller('nimOptionsController', ['$scope', '$window', function($scope, $window) {
         $scope.bg = $window.chrome.extension.getBackgroundPage().angular.element('#nim').scope();
+        var $ = $window.$;
 
-        $window.chrome.windows.onFocusChanged.addListener(function focusChangedListener() {
-            console.log("running focusChangedListener");
+        $($window).blur(function() {
             $scope.bg.$emit('options-window-focusChanged');
         });
         var slider = $window.document.getElementById('checkInterval');
@@ -42,14 +42,14 @@ ngApp
             tooltips: true
         });
         var rangeSliderValueElement = $window.document.getElementById('checkInterval-value');
-        slider.noUiSlider.on('update', function (values, handle) {
+        slider.noUiSlider.on('update', function(values, handle) {
             rangeSliderValueElement.innerHTML = values[handle];
             $scope.bg.settings.checkInterval = parseInt(values[handle]);
         });
-        slider.noUiSlider.on('set', function (values, handle) {
+        slider.noUiSlider.on('set', function(values, handle) {
             $window._gaq.push(['_trackEvent', 'checkInterval-value', values[handle]]);
         });
-        $scope.saveButtonHandler = function () {
+        $scope.saveButtonHandler = function() {
             $window._gaq.push(['_trackEvent', 'save button', 'clicked']);
             $scope.bg.$emit('options-window-closed');
             $window.close();
