b32ef014b06c42335048ce6d2f30f26f2707f089
667 <667@june07.com>
2016-12-19 14:11:41 -0800

    no message

--- js/googleanalytics.js
+++ js/googleanalytics.js
@@ -1,6 +1,8 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-87646107-1']);
 _gaq.push(['_trackPageview']);
+_gaq.push(['_setCampSourceKey', 'com.june07']);
+_gaq.push(['_setCampMediumKey', 'nim']);
 
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
