dd682d51ab28133f9754b55fd0452c614863fd8a
will@onezerohosting.com <will@onezerohosting.com>
2020-09-14 11:38:02 -0700

    Updated materialcss dep code, blurb, moved js import to bottom

--- devtools-panel.js
+++ devtools-panel.js
@@ -82,10 +82,8 @@ ngApp
                         outDuration: 225,
                         constrainWidth: false, // Does not change width of dropdown to that of the activator
                         hover: true, // Activate on hover
-                        gutter: 0, // Spacing from edge
-                        belowOrigin: true, // Displays dropdown below the button
-                        alignment: 'left', // Displays dropdown with edge aligned to the left of button
-                        stopPropagation: false // Stops event propagation
+                        coverTrigger: false, // Displays dropdown below the button
+                        alignment: 'left' // Displays dropdown with edge aligned to the left of button
                     });
                     loadMessages(); break;
                 case 'brakecode-login-cancelled':
@@ -102,10 +100,8 @@ ngApp
                 outDuration: 225,
                 constrainWidth: false, // Does not change width of dropdown to that of the activator
                 hover: true, // Activate on hover
-                gutter: 0, // Spacing from edge
-                belowOrigin: true, // Displays dropdown below the button
-                alignment: 'left', // Displays dropdown with edge aligned to the left of button
-                stopPropagation: false // Stops event propagation
+                coverTrigger: false, // Displays dropdown below the button
+                alignment: 'left' // Displays dropdown with edge aligned to the left of button
             });
             if ($scope.bg.nodeReportSortedMessages) {
                 $scope.$apply();
